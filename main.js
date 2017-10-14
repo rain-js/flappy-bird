5df3444221f230526c231fa1fde048495591aa0c
rain-js <unique_ritchie@163.com>
2017-10-15 03:44:31 +0800

    :hammer: add file

---
+++ main.js
@@ -0,0 +1,82 @@
+var blocks = [];
+var enableDebugMode = function(enable) {
+	if (!enable) {
+		return;
+	}
+
+	window.addEventListener('keydown', function(event) {
+		var k = event.key;
+
+		if ('123456789'.includes(k)) {
+			log(loadLevel(k))
+			blocks = loadLevel(k);
+		}
+	});
+
+	document.querySelector('#id-input-fps').addEventListener('input', function(event) {
+		// log(event.target)
+		log('fps = ' + window.fps);
+		window.fps = event.target.value;
+	});
+};
+
+var __main = function() {
+	enableDebugMode(true);
+	
+	var game = BirdGame();
+	var bird = Bird();
+	var ball = Ball();
+	var score = 0;
+	
+	blocks = loadLevel(1);
+
+	game.registerActions('a', function() {
+		bird.moveLeft();
+	});
+
+	game.registerActions('d', function() {
+		bird.moveRight();
+	});
+
+	game.registerActions('f', function() {
+		ball.fire();
+	});
+
+	game.registerActions('p', function() {
+		ball.pause();
+	});
+
+	game.update = function() {
+		// 挡板与球
+		if (bird.collide(ball)) {
+			ball.bounce();
+		}
+		ball.move();
+
+		// 球与砖块
+		for (var i = 0; i < blocks.length; i++) {
+			// 砖块存在时再判断相撞
+			if (blocks[i].alive && ball.collide(blocks[i])) {
+				blocks[i].kill();
+				ball.bounce();
+				score += 100;
+			}	
+		}
+		
+	}
+
+	game.draw = function() {
+		game.drawImage(ball);
+		game.drawImage(bird);
+
+		for (var i = 0; i <  blocks.length; i++) {
+			if (blocks[i].alive) {
+				game.drawImage(blocks[i]);		
+			}	
+		}
+
+		game.context.font = '18px serif';
+	 	game.context.fillText('Score：' + score, 10, 485);
+		
+	};
+}
